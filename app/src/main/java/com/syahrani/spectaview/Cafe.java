5590e833f70c4f3fda8e2c6d8144aebe881714cb
syahraniinh <syahraninh@mhs.mdp.ac.id>
2023-06-21 18:52:50 -0700

    DONE

---
+++ app/src/main/java/com/syahrani/spectaview/Cafe.java
@@ -0,0 +1,67 @@
+package com.syahrani.spectaview;
+
+public class Cafe {
+    private String id;
+    private String nama_cafe;
+    private String deskripsi_cafe;
+    private String lokasi_cafe;
+    private String rating_cafe;
+    private String jam_operasional;
+    private String link_foto;
+
+    public String getId() 
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama_cafe() {
+        return nama_cafe;
+    }
+
+    public void setNama_cafe(String nama_cafe) {
+        this.nama_cafe = nama_cafe;
+    }
+
+    public String getDeskripsi_cafe() {
+        return deskripsi_cafe;
+    }
+
+    public void setDeskripsi_cafe(String deskripsi_cafe) {
+        this.deskripsi_cafe = deskripsi_cafe;
+    }
+
+    public String getLokasi_cafe() {
+        return lokasi_cafe;
+    }
+
+    public void setLokasi_cafe(String lokasi_cafe) {
+        this.lokasi_cafe = lokasi_cafe;
+    }
+
+    public String getRating_cafe() {
+        return rating_cafe;
+    }
+
+    public void setRating_cafe(String rating_cafe) {
+        this.rating_cafe = rating_cafe;
+    }
+
+    public String getJam_operasional() {
+        return jam_operasional;
+    }
+
+    public void setJam_operasional(String jam_operasional) {
+        this.jam_operasional = jam_operasional;
+    }
+
+    public String getLink_foto() {
+        return link_foto;
+    }
+
+    public void setLink_foto(String link_foto) {
+        this.link_foto = link_foto;
+    }
+}
